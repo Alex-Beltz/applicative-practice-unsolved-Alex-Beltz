20b1cf5683567ff5b24d40d70d9065b24db14d3c
Alex.Beltz <97088270+Alex-Beltz@users.noreply.github.com>
2023-01-08 09:14:37 +0800

    Update e17.js
    refactored to better code (I think), but used the original to pass the test

--- exercises/e17.js
+++ exercises/e17.js
@@ -1,4 +1,7 @@
-export function minBy(array, cb) {
+/*this works fine when I move the array over and add duplicate oldest age and youngest age objects, 
+and with an empty array but won't pass the test for some reason*/
+
+/*export function minBy(array, cb) {
 let ages = array.map(p => p.age)
   let lowest = cb(ages).shift()
   return array.filter(p => p.age === lowest)[0]
@@ -12,4 +15,24 @@ let ages = array.map(p => p.age)
 
 function cb(ages) {
   return ages.sort()
+}*/
+
+export function minBy(array, cb) {
+  let lowestAge = array[0];
+  for (let person of array) {
+    if (cb(person) < lowestAge.age) {
+      return person;
+    }
+  }
+  return lowestAge;
+}
+
+export function maxBy(array, cb) {
+  let highestAge = array[0];
+  for (let person of array) {
+    if (cb(person) > cb(highestAge)) {
+      return person;
+    }
+  }
+  return highestAge;
 }
